415c1a750014fce86851eb4721382e5ecfc3815c
Renishchandera <renishchandera1307@gmail.com>
2024-01-09 21:58:03 +0530

    changing paths again to absolute

--- app.js
+++ app.js
@@ -7,29 +7,29 @@ contactMeBtn.addEventListener('click', ()=>{
 let myProjectsBtn = document.getElementsByClassName('projects-btn');
 if(myProjectsBtn.length > 0){
     myProjectsBtn[0].addEventListener('click', ()=>{
-        window.location.href = "../projects-work.html";
+        window.location.href = "https://renishchandera.github.io/MyPortfolio/projects-work.html";
     });
     myProjectsBtn[1].addEventListener('click', ()=>{
-        window.location.href = "../projects-work.html";
+        window.location.href = "https://renishchandera.github.io/MyPortfolio/projects-work.html";
     });
 }
 
 let myEducationBtn = document.getElementsByClassName('education-btn');
 if(myEducationBtn.length > 0){
     myEducationBtn[0].addEventListener('click', ()=>{
-        window.location.href = "../education-certificates.html";
+        window.location.href = "https://renishchandera.github.io/MyPortfolio/education-certificates.html";
     });
     myEducationBtn[1].addEventListener('click', ()=>{
-        window.location.href = "../education-certificates.html";
+        window.location.href = "https://renishchandera.github.io/MyPortfolio/education-certificates.html";
     });
 }
 
 let homeBtn = document.getElementsByClassName('home-btn');
 if(homeBtn.length > 0){
     homeBtn[0].addEventListener('click', ()=>{
-        window.location.href = "../index.html";
+        window.location.href = "https://renishchandera.github.io/MyPortfolio/index.html";
     });
     homeBtn[1].addEventListener('click', ()=>{
-        window.location.href = "../index.html";
+        window.location.href = "https://renishchandera.github.io/MyPortfolio/index.html";
     });
 }
