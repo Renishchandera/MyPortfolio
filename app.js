3ef96356dc8cd6081efcc13b86905442fed42e56
Renishchandera <renishchandera1307@gmail.com>
2024-01-09 21:39:35 +0530

    simmilliar problem solving as previous and changes path to relative

--- app.js
+++ app.js
@@ -6,24 +6,24 @@ contactMeBtn.addEventListener('click', ()=>{
 
 let myProjectsBtn = document.getElementsByClassName('projects-btn');
 myProjectsBtn[0].addEventListener('click', ()=>{
-    window.location.href = "https://renishchandera.github.io/MyPortfolio/projects-work.html";
+    window.location.href = "../projects-work.html";
 });
 myProjectsBtn[1].addEventListener('click', ()=>{
-    window.location.href = "https://renishchandera.github.io/MyPortfolio/projects-work.html";
+    window.location.href = "../projects-work.html";
 });
 
 let myEducationBtn = document.getElementsByClassName('education-btn');
 myEducationBtn[0].addEventListener('click', ()=>{
-    window.location.href = "https://renishchandera.github.io/MyPortfolio/education-certificates.html";
+    window.location.href = "../education-certificates.html";
 });
 myEducationBtn[1].addEventListener('click', ()=>{
-    window.location.href = "https://renishchandera.github.io/MyPortfolio/education-certificates.html";
+    window.location.href = "../education-certificates.html";
 });
 
 let homeBtn = document.getElementsByClassName('home-btn');
 homeBtn[0].addEventListener('click', ()=>{
-    window.location.href = "https://renishchandera.github.io/MyPortfolio/";
+    window.location.href = "../index.html";
 });
 homeBtn[1].addEventListener('click', ()=>{
-    window.location.href = "https://renishchandera.github.io/MyPortfolio/";
+    window.location.href = "../index.html";
 });
