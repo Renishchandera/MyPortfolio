3f3896710901d9df68d752579e96ca491e54c494
Renishchandera <renishchandera1307@gmail.com>
2024-01-09 21:52:19 +0530

    trying if statement to check if class exist to solve the previous problem;(

--- app.js
+++ app.js
@@ -5,25 +5,31 @@ contactMeBtn.addEventListener('click', ()=>{
 });
 
 let myProjectsBtn = document.getElementsByClassName('projects-btn');
-myProjectsBtn[0].addEventListener('click', ()=>{
-    window.location.href = "../projects-work.html";
-});
-myProjectsBtn[1].addEventListener('click', ()=>{
-    window.location.href = "../projects-work.html";
-});
+if(myProjectsBtn.length > 0){
+    myProjectsBtn[0].addEventListener('click', ()=>{
+        window.location.href = "../projects-work.html";
+    });
+    myProjectsBtn[1].addEventListener('click', ()=>{
+        window.location.href = "../projects-work.html";
+    });
+}
 
 let myEducationBtn = document.getElementsByClassName('education-btn');
-myEducationBtn[0].addEventListener('click', ()=>{
-    window.location.href = "../education-certificates.html";
-});
-myEducationBtn[1].addEventListener('click', ()=>{
-    window.location.href = "../education-certificates.html";
-});
+if(myEducationBtn.length > 0){
+    myEducationBtn[0].addEventListener('click', ()=>{
+        window.location.href = "../education-certificates.html";
+    });
+    myEducationBtn[1].addEventListener('click', ()=>{
+        window.location.href = "../education-certificates.html";
+    });
+}
 
 let homeBtn = document.getElementsByClassName('home-btn');
-homeBtn[0].addEventListener('click', ()=>{
-    window.location.href = "../index.html";
-});
-homeBtn[1].addEventListener('click', ()=>{
-    window.location.href = "../index.html";
-});
+if(homeBtn.length > 0){
+    homeBtn[0].addEventListener('click', ()=>{
+        window.location.href = "../index.html";
+    });
+    homeBtn[1].addEventListener('click', ()=>{
+        window.location.href = "../index.html";
+    });
+}
