31bedc17a32c35babc9469d768b207311bd5db1f
Renishchandera <renishchandera1307@gmail.com>
2024-01-09 20:47:41 +0530

    added the webpage changing path using javascript window.lacation.href = 'path', for education and projects and home page

--- app.js
+++ app.js
@@ -3,3 +3,18 @@ let contactMeSection = document.getElementById('contact-section');
 contactMeBtn.addEventListener('click', ()=>{
     contactMeSection.scrollIntoView({behavior: 'smooth'});
 });
+
+let myProjectsBtn = document.getElementsByClassName('projects-btn');
+myProjectsBtn.addEventListener('click', ()=>{
+    window.location.href = "https://renishchandera.github.io/MyPortfolio/projects-work.html";
+});
+
+let myEducationBtn = document.getElementsByClassName('education-btn');
+myEducationBtn.addEventListener('click', ()=>{
+    window.location.href = "https://renishchandera.github.io/MyPortfolio/education-certificates.html";
+});
+
+let homeBtn = document.getElementsByClassName('home-btn');
+homeBtn.addEventListener('click', ()=>{
+    window.location.href = "https://renishchandera.github.io/MyPortfolio/";
+});
