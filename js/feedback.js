64df0a8903cb288417bf2696919bd15405193e67
Renishchandera <renishchandera1307@gmail.com>
2025-01-28 21:43:08 +0530

    Changed the email send function to emailjs.send from emailjs.sendForm. Tried TO Fix

--- js/feedback.js
+++ js/feedback.js
@@ -11,7 +11,12 @@ event.preventDefault(); // Prevent the default form submission
 const serviceID = 'service_x3r85sb';
 const templateID = 'template_twp24z7';
 
-emailjs.send(serviceID, templateID, this)
+emailjs.send(serviceID, templateID, {
+    to_name: "Renish",
+    from_name: document.getElementById("name").value,
+    message: document.getElementById("message").value,
+    reply_to: document.getElementById("email").value
+})
    .then(function(response) {
        console.log('SUCCESS!', response.status, response.text);
        alert('Thank you for your feedback!');
