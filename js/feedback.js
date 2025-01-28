a0b0aca91519aea738024c755efb98ad2fd3497a
Renishchandera <renishchandera1307@gmail.com>
2025-01-28 22:07:32 +0530

    Changed The emailjs.send to emailjs.sendForm again

--- js/feedback.js
+++ js/feedback.js
@@ -11,12 +11,17 @@ event.preventDefault(); // Prevent the default form submission
 const serviceID = 'service_x3r85sb';
 const templateID = 'template_twp24z7';
 
-emailjs.send(serviceID, templateID, {
-    to_name: "Renish",
-    from_name: document.getElementById("name").value,
-    message: document.getElementById("feedback").value,
-    reply_to: document.getElementById("email").value
-})
+
+//Send data Another Way
+// emailjs.send(serviceID, templateID, {
+//     to_name: "Renish",
+//     from_name: document.getElementById("name").value,
+//     message: document.getElementById("feedback").value,
+//     reply_to: document.getElementById("email").value
+// })
+
+//Send Form directly
+emailjs.sendForm(serviceID, templateID, this)
    .then(function(response) {
        console.log('SUCCESS!', response.status, response.text);
        alert('Thank you for your feedback!');
