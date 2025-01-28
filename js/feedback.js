ab6a02afde20e8d570cd14551784be4f5cc25f50
Renishchandera <renishchandera1307@gmail.com>
2025-01-28 21:25:32 +0530

    Script Added to the file js/feedback.js

---
+++ js/feedback.js
@@ -0,0 +1,22 @@
+(function() {
+    emailjs.init("2CKA4I2jR7lutzZSJ");
+ })();
+
+
+ //Send Email 
+
+ document.getElementById('form').addEventListener('submit', function(event) {
+event.preventDefault(); // Prevent the default form submission
+
+const serviceID = 'service_x3r85sb';
+const templateID = 'template_twp24z7';
+
+emailjs.sendForm(serviceID, templateID, this)
+   .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+       alert('Thank you for your feedback!');
+   }, function(error) {
+       console.log('FAILED...', error);
+       alert('Oops! Something went wrong.');
+   });
+});
