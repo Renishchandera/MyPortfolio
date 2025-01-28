8fb8fdd47151810dcb2bcbf01202d95011976a30
Renishchandera <renishchandera1307@gmail.com>
2025-01-28 21:46:06 +0530

    Final Try

--- js/feedback.js
+++ js/feedback.js
@@ -14,7 +14,7 @@ const templateID = 'template_twp24z7';
 emailjs.send(serviceID, templateID, {
     to_name: "Renish",
     from_name: document.getElementById("name").value,
-    message: document.getElementById("message").value,
+    message: document.getElementById("feedback").value,
     reply_to: document.getElementById("email").value
 })
    .then(function(response) {
